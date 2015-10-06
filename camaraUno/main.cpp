2009b1e6a4f53991fbb7e80d16ac679bdfe66cb0
cj3cervantes <cj.3.cervantes@gmail.com>
2015-10-06 13:12:00 -0500

    primera version

---
+++ camaraUno/main.cpp
@@ -0,0 +1,50 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world! Claudia" << endl;
+    return 0;
+}
+*/
+
+
+/*
+#include <cv.h>
+#include <highgui.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cvNamedWindow("Hello", 1);
+    cvWaitKey(0);
+    cvDestroyWindow("Hello");
+    return 0;
+}
+*/
+
+
+#include <stdlib.h>
+#include <cv.hpp>
+#include <cxcore.hpp>
+#include <highgui.h>
+
+int main(int argc, char** argv)
+{
+    cv::Mat frame;
+    int c;
+    CvCapture *capture = cvCaptureFromCAM(0);
+    while(1)
+    {
+        frame = cvQueryFrame(capture);
+        cv::imshow("OpenCV",frame);
+
+        c = cv::waitKey(10);
+        if(c==27)
+            break;
+    }
+    return 0;
+}
